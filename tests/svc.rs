452f7661de8c8d15f6241987a412e5a37a004ecc
Kenneth Fossen <fossen.kenneth@gmail.com>
2021-11-03 14:25:20 +0100

    extra tests for expantions

---
+++ tests/svc.rs
@@ -0,0 +1,13 @@
+use capabilities::PoolSqlite;
+use capabilities_derive::service;
+use sqlx::Pool;
+
+#[service(PoolSqlite)]
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let connection_string = "sqlite::memory:".to_string();
+    let _pool = CapService::build(connection_string)
+        .await
+        .expect("Failed to create database");
+    Ok(())
+}
