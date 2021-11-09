67a8285d22d5cbec04ba52cce2c9ad9b100c2acc
Kenneth <fossen.kenneth@gmail.com>
2021-11-09 21:53:27 +0100

    tests

---
+++ tests/svc_cap.rs
@@ -0,0 +1,21 @@
+use capabilities::PoolSqlite;
+use capabilities_derive::service;
+use capabilities_derive::capabilities;
+use sqlx::Pool;
+
+
+#[capabilities(Read)]
+struct Orders {
+    id: i32, 
+    name: String, 
+}
+
+#[service(PoolSqlite)]
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let connection_string = "sqlite::memory:".to_string();
+    let _pool = CapService::build(connection_string)
+        .await
+        .expect("Failed to create database");
+    Ok(())
+}
