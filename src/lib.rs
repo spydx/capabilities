701d6fcda1579f2dbc37560e8461e05f6fb607c7
Kenneth Fossen <fossen.kenneth@gmail.com>
2021-11-10 19:39:12 +0100

    format

--- src/lib.rs
+++ src/lib.rs
@@ -20,18 +20,25 @@ pub enum Operations {
 }
 
 #[allow(dead_code)]
-pub struct Create<T>{data:T}
+pub struct Create<T> {
+    data: T,
+}
 #[allow(dead_code)]
-pub struct Read<T>{data:T}
+pub struct Read<T> {
+    data: T,
+}
 #[allow(dead_code)]
-pub struct Update<T>{data:T}
+pub struct Update<T> {
+    data: T,
+}
 #[allow(dead_code)]
-pub struct Delete<T>{data:T}
+pub struct Delete<T> {
+    data: T,
+}
 
 pub struct ReadAll;
 pub struct UpdateAll;
 pub struct DeleteAll;
-
 
 pub type PoolSqlite = Pool<Sqlite>;
 pub type PoolPostgres = Pool<Postgres>;
